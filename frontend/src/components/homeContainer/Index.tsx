dab51685e41ad6a0ee5ddb3a1ff2e9b9ca9c916d
Giovanni Janial <giovannijanial@gmail.com>
2022-06-22 18:26:19 -0300

    create prod container

--- frontend/src/components/homeContainer/Index.tsx
+++ frontend/src/components/homeContainer/Index.tsx
@@ -1,24 +1,36 @@
 
 import BoxMid from './BoxMid'
 import BoxRight from './BoxRigth'
-import { StyledAside, StyledContainer, StyledGridMain, StyledMid, StyledRight } from './index.styled'
+import { StyledAside, StyledContainer, StyledGridItem, StyledGridMain, StyledGridMid, StyledMid, StyledProductBox, StyledRight } from './index.styled'
 import ListCategories from './ListCategories'
 
 const HomeContainer = () => {
   return (
-    <StyledContainer>
-      <StyledGridMain container spacing={{ xs: 4 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-        <StyledAside item md={3} display={{ sm: "none", md: "block", xs: "none" }}>
-          <ListCategories />
-        </StyledAside>
-        <StyledMid item md={5} sm={8} xs={12}>
-          <BoxMid />
-        </StyledMid>
-        <StyledRight item md={4} sm={8} xs={12}>
-          <BoxRight />
-        </StyledRight>
-      </StyledGridMain>
-    </StyledContainer >
+    <>
+      <StyledContainer>
+        <StyledGridMain container spacing={{ xs: 4 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          <StyledAside item md={3} display={{ sm: "none", md: "block", xs: "none" }}>
+            <ListCategories />
+          </StyledAside>
+          <StyledMid item md={5} sm={8} xs={12}>
+            <BoxMid />
+          </StyledMid>
+          <StyledRight item md={4} sm={8} xs={12}>
+            <BoxRight />
+          </StyledRight>
+        </StyledGridMain>
+        <StyledGridMid container spacing={{ xs: 4 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          <StyledGridItem>
+            <StyledProductBox>
+              <p>prod</p>
+            </StyledProductBox>
+          </StyledGridItem>
+        </StyledGridMid>
+
+      </StyledContainer >
+
+    </>
+
   )
 }
 
--- frontend/src/components/homeContainer/index.styled.ts
+++ frontend/src/components/homeContainer/index.styled.ts
@@ -1,4 +1,4 @@
-import { Container, Grid, styled } from "@mui/material";
+import { Box, Container, Grid, styled } from "@mui/material";
 import { theme } from "../../app.styled";
 
 export const StyledContainer = styled(Container)`
@@ -12,3 +12,9 @@ export const StyledAside = styled(Grid)``;
 export const StyledMid = styled(Grid)``;
 
 export const StyledRight = styled(Grid)``;
+
+export const StyledGridMid = styled(Grid)``;
+
+export const StyledGridItem = styled(Grid)``;
+
+export const StyledProductBox = styled(Box)``;
